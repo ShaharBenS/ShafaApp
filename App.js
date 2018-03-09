74ff0f179b89d2131f3e8342d2cc5e1a9a854348
Shahar <shahar0897@gmail.com>
2018-03-09 21:59:27 +0200

    facebook profile picture

--- App.js
+++ App.js
@@ -10,49 +10,58 @@ import {
     StyleSheet,
     Text,
     View,
-    Alert
+    Alert,
+    Image
 } from 'react-native';
 
 import {FBLogin, FBLoginManager} from 'react-native-facebook-login';
 
 type Props = {};
 export default class App extends Component<Props> {
+    constructor(props){
+        super(props);
+        this.state = {user:null}
+    }
+
     render() {
         let _this = this;
         return (
             <View style = {{alignItems:'flex-end',flexDirection:'column'}}>
-                <FBLogin style={{height:100 }}
+                <Image source={_this.state.user == null ?
+                    {uri:'http://www.solidbackgrounds.com/851x315-baby-powder-solid-color-background.html'} :
+                    {uri:'https://graph.facebook.com/'+_this.state.user.userId+'/picture?type=large'}}
+                style = {{height:120,width:120}}/>
+                <Text>{
+                    _this.state.user == null ? 0 : _this.state.user.userId
+                }</Text>
+                <FBLogin
                            ref={(fbLogin) => { this.fbLogin = fbLogin }}
                            permissions={["email","user_friends"]}
                            loginBehavior={FBLoginManager.LoginBehaviors.Native}
                            onLogin={function(data){
-                               Alert.alert("Logged in!");
-                               Alert.log(data);
+                               Alert.alert('Logged In!',JSON.stringify(data));
                                _this.setState({ user : data.credentials });
                            }}
                            onLogout={function(){
-                               console.log("Logged out.");
+                               Alert.alert("Logged out.");
                                _this.setState({ user : null });
                            }}
                            onLoginFound={function(data){
-                               console.log("Existing login found.");
-                               console.log(data);
+                               Alert.alert("Existing login found.",JSON.stringify(data));
                                _this.setState({ user : data.credentials });
                            }}
                            onLoginNotFound={function(){
-                               console.log("No user logged in.");
+                               Alert.alert("No user logged in.");
                                _this.setState({ user : null });
                            }}
                            onError={function(data){
-                               console.log("ERROR");
-                               console.log(data);
+                               Alert.alert("ERROR",JSON.stringify(data));
                            }}
                            onCancel={function(){
-                               console.log("User cancelled.");
+                               Alert.alert("User cancelled.");
                            }}
                            onPermissionsMissing={function(data){
-                               console.log("Check permissions!");
-                               console.log(data);
+                               Alert.alert("Check permissions!",JSON.stringify(data));
                            }}
             /></View>
         );
