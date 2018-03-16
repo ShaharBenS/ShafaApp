83240f1e3ba7a008b6c6d6fac34eb5e226feec53
omriattiya <omri206attiya@gmail.com>
2018-03-16 11:01:16 +0200

    category almost done

--- App.js
+++ App.js
@@ -8,31 +8,34 @@ import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
-    Image,
-    Text,
     TouchableOpacity,
-    ScrollView,
-    Dimensions
+    FlatList,
 
 } from 'react-native';
 import {CategoryItem} from './App/Components/CategoryItem';
 
 export default class App extends Component<Props> {
+
+
     render() {
         let pic = require('./App/Images/placeholder.jpg');
+        let name = "Category i"
+        let data=[{pict:pic,namet:name, id:'0'},
+            {pict:pic,namet:name, id:'1'},
+            {pict:pic,namet:name, id:'2'},
+            {pict:pic,namet:name, id:'3'},
+            {pict:pic,namet:name, id:'4'},
+            {pict:pic,namet:name, id:'5'},
+            {pict:pic,namet:name, id:'6'},
+            {pict:pic,namet:name, id:'7'},];
         return (
-                <ScrollView contentContainerStyle={{ height:2000}} >
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                    <CategoryItem categoryImagePath={pic} categoryName="T-Shirts"/>
-                </ScrollView>
+            <View style={styles.container}>
+            <FlatList
+                data={data}
+                renderItem={({item})=><CategoryItem categoryImagePath={item.pict} categoryName={item.namet}/>}
+                keyExtractor={item => item.id}
+            />
+            </View>
         );
     }
 };
@@ -41,7 +44,6 @@ export default class App extends Component<Props> {
     const styles = StyleSheet.create({
         container: {
         flex: 1,
-
         },
 
     });
