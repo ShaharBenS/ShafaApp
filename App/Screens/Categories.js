7af56df2db8c52aa7f1ef1aa6df087a49a08868d
omriattiya <omri206attiya@gmail.com>
2018-03-16 21:08:43 +0200

    moved from App.js to Categories.js

--- App/Screens/Categories.js
+++ App/Screens/Categories.js
@@ -1,40 +1,63 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
 import React, { Component } from 'react';
 import {
     StyleSheet,
     View,
+    FlatList,
     Image,
-    Text,
-    TouchableOpacity,
-    AppRegistry
-} from 'react-native';
+    Dimensions,
 
-class Categories extends Component<props> {
+} from 'react-native';
+import {CategoryItem} from './App/Components/CategoryItem';
+
+export default class Categories extends Component<Props> {
+
+
     render() {
+        let pic = require('./App/Images/placeholder.jpg');
+        let name = "Category Name";
+        let data=[{pict:pic,namet:name, id:'0'},
+            {pict:pic,namet:name, id:'1'},
+            {pict:pic,namet:name, id:'2'},
+            {pict:pic,namet:name, id:'3'},
+            {pict:pic,namet:name, id:'4'},
+            {pict:pic,namet:name, id:'5'},
+            {pict:pic,namet:name, id:'6'},
+            {pict:pic,namet:name, id:'7'},];
         return (
-            <TouchableOpacity activeOpacity={0.5} style={styles.container}>
-                <Image source={require(this.props.categorySource)} style={styles.image}/>
-                <Text style={styles.title}>{this.props.categoryName}</Text>
-            </TouchableOpacity>
+            <View style={styles.container}>
+
+                <FlatList
+                    data={data}
+                    renderItem={({item})=><CategoryItem categoryImagePath={item.pict} categoryName={item.namet}/>}
+                    keyExtractor={item => item.id}
+                    ListHeaderComponent={<Image source={require('./App/Images/logoPlaceholder.png')} style={styles.logo}/>}
+                />
+            </View>
         );
     }
-}
+};
 
+
+const percent = 0.1;
+const window = Dimensions.get('window');
+const renderedHeight = percent*window.height;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 0,
+        flex: 1,
+        backgroundColor: '#EFEFEF'
+    },
+    logo : {
+        resizeMode:'center',
+        height: renderedHeight,
         margin: 10,
-        height:'40%',
-        backgroundColor:'#EFEFEF'
+        alignSelf:'center'
+
     },
-    image: {
-        width:'100%',
-        height:'80%',
-        resizeMode:'cover',
-    },
-    title: {
-        fontSize: 30,
-        textAlign: 'center',
-        padding: 5
-    }
 });
